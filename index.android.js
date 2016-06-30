7884caf3545e4f7bb458ee762f213e8a1b7d1440
zhangzequn <776021361@qq.com>
2016-06-30 18:33:44 +0800

    【Add】创建初始项目

---
+++ index.android.js
@@ -0,0 +1,60 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+'use strict';
+
+import React from 'react';
+import AppMain from './AppMain';
+import {AppRegistry} from 'react-native';
+
+AppRegistry.registerComponent('ReduxLearn', () => AppMain);
+
+// 初始项目
+// import React, { Component } from 'react';
+// import {
+//   AppRegistry,
+//   StyleSheet,
+//   Text,
+//   View
+// } from 'react-native';
+
+// class ReduxLearn extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit index.android.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           Shake or press menu button for dev menu
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+
+// AppRegistry.registerComponent('ReduxLearn', () => ReduxLearn);
---
+++ src/AppMain.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {Provider} from 'react-redux';
+import configureStore from './store/configure-store';
+
+import App from './containers/app';
+
+const store = configureStore();
+
+class AppContent extends Component {
+	render() {
+		return (
+			<Provider store={store}>
+				<App/>
+			</Provider>
+		)
+	}
+}
+
+export default AppContent;
